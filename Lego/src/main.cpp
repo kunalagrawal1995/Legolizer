f55713ec9e36106e67f75364ecd5fd28e13526c4
Sivaprasad Sudhir <sivaprasad@Sivaprasads-MacBook-Pro.local>
2017-05-01 15:04:35 +0530

    Added colors for different lego blocks

--- Lego/src/main.cpp
+++ Lego/src/main.cpp
@@ -14,24 +14,24 @@ int main(int argc, char *argv[]){
 	read_lego_bricks("src/legoBricksConfig.txt");
 	cout << "LEGO BRICKS READ" << endl;
 
-	graph.merge();
-	set<Node*> temp = graph.find_articulation_points();
-	cout << "PRINTING ARTICULATION POINTS.." << endl;
-	for(auto iter: temp){
-		iter->print();
-	}
+	// graph.merge();
+	// set<Node*> temp = graph.find_articulation_points();
+	// cout << "PRINTING ARTICULATION POINTS.." << endl;
+	// for(auto iter: temp){
+	// 	iter->print();
+	// }
 	
-	if(temp.size() > 0){
-		graph.remove_articulation_point(*(temp.begin()));
-	}
-	cout << "ARTICULATION POINTS REMOVAL ATTEMPTED" << endl;
+	// if(temp.size() > 0){
+	// 	graph.remove_articulation_point(*(temp.begin()));
+	// }
+	// cout << "ARTICULATION POINTS REMOVAL ATTEMPTED" << endl;
 	
-	set<Node*> temp1 = graph.find_articulation_points();
-	cout << "PRINTING ARTICULATION POINTS.." << endl;
-	for(auto iter: temp1){
-		iter->print();
-	}
-	cout << "DONE" << endl;
+	// set<Node*> temp1 = graph.find_articulation_points();
+	// cout << "PRINTING ARTICULATION POINTS.." << endl;
+	// for(auto iter: temp1){
+	// 	iter->print();
+	// }
+	// cout << "DONE" << endl;
 	// graph.print();
 	Viewer viewer;
 	viewer.setObject(&graph);
